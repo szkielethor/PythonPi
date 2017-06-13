77e3e161bed14d8b762c47736ba6ca45715b22fb
Bartłomiej Łukaszewski <bartluk@o2.pl>
2017-06-13 19:08:34 +0200

    Delete while_loop.py

--- while_loop.py
+++
@@ -1,7 +0,0 @@
-count = 0
-if count < 5:
-    print "hello, I am an if statement and coutn is", count
-
-    while count <10:
-        print"hello, I am a while and count is", count
-        count += 1
